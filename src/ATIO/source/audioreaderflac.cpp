a2b31a5bb3d71ce943b8317e50e68854bb789da0
idunmore <62761638+idunmore@users.noreply.github.com>
2026-04-07 20:22:34 -0400

    Fix missing _lzcnt_u32() on x86/x64 w/ clang

--- src/ATIO/source/audioreaderflac.cpp
+++ src/ATIO/source/audioreaderflac.cpp
@@ -14,6 +14,7 @@
 //	You should have received a copy of the GNU General Public License along
 //	with this program. If not, see <http://www.gnu.org/licenses/>.
 
+#include <intrin.h>
 #include <stdafx.h>
 #include <vd2/system/binary.h>
 #include <vd2/system/cpuaccel.h>
